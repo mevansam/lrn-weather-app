73fd30d02c906915f477625c8feaaa9d4a2bbdc4
Mevan Samaratunga <mevansam@gmail.com>
2018-06-21 18:51:39 +0400

    removed warnings from view

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import WeatherApp from './src/Main';
 
+console.disableYellowBox = true;
+
 AppRegistry.registerComponent('LRN_Weather_App', () => WeatherApp);
